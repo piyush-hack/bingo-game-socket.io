30d790197000c2ee15963126553f00b544e14fb4
Piyush Puniya <75742731+piyush-hack@users.noreply.github.com>
2022-03-28 19:50:15 +0530

    bingo game

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require("express");
 var cors = require("cors");
 var bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+var path = require('path');
+
+
 const app = express();
 const port = process.env.PORT || 5000;
 const http = require("http");
@@ -13,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/')));
+
 
 let rooms = {};
 let roomDetails = {};
